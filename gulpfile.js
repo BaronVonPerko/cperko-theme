0ba62d9ce1ba4d349d9659bc4ea3b9cb38b06d5e
Chris Perko <cperko@teachers-teachers.com>
2017-02-01 10:48:22 -0600

    Init theme

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp        = require("gulp"),
+    browserSync = require("browser-sync"),
+    concatCss   = require("gulp-concat-css"),
+    cleanCss    = require("gulp-clean-css"),
+    del         = require("del");
+    
+    
+gulp.task('sync', ['css'], function() {
+    var files = ['**/*.css', '**/**.php', '**/**.js'];
+    
+    browserSync.init(files, {
+        proxy: 'http://localhost/wordpress',
+        port: 8082,
+        injectChanges: true,
+        notify: false
+    });
+
+    gulp.watch('./inc/css/*.css', ['css']);
+    gulp.watch('./**/*.php').on('change', browserSync.reload);
+});
+
+
+gulp.task('css', function() {
+    gulp.src('inc/**/*.css')
+        .pipe(concatCss('custom.css'))
+        .pipe(gulp.dest('dist/'));
+
+    return gulp.src('dist/custom.css')
+        .pipe(cleanCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist/minified'))
+        .pipe(browserSync.stream());
+});
