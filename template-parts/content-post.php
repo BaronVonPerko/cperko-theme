eb3e306b9c27b749b1b7f1aaa292e217fac2f5ce
cperko <cperko@teachers-teachers.com>
2017-03-16 17:50:05 -0500

    Fixing post content

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -26,15 +26,20 @@
 		endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content post-content">
 		<?php
 			// the_content( sprintf(
 			// 	/* translators: %s: Name of current post. */
 			// 	wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'cperko' ), array( 'span' => array( 'class' => array() ) ) ),
 			// 	the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			// ) );
+			?>
 
-			the_post_thumbnail('full', array( 'class' => 'post-img' ));
+			<div class="post-excerpt-image-wrapper">
+				<?php the_post_thumbnail('large', array( 'class' => 'post-featured-img' )); ?>
+			</div>
+
+			<?php 
 			the_content();
 
 			wp_link_pages( array(
